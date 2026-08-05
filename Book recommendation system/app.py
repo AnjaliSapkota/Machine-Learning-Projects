a4fe3c8560cba820f77372b418e4f14f19d22f59
AnjaliSapkota <sapkotaanjali123@gmail.com>
2026-08-05 12:01:26 +0545

    ft: added popular books page

--- Book recommendation system/app.py
+++ Book recommendation system/app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 import pickle
 
 # Page Configuration
@@ -12,14 +13,61 @@ st.set_page_config(
 st.write("Welcome to my Book Recommendation System!")
 
 # Load models
+def load_data():
+    popular_books = pd.read_csv("models/popular_books.csv")
+    books = pd.read_pickle("models/books.pkl")
+    book_pivot = pd.read_pickle("models/book_pivot.pkl")
 
-popular_books = pd.read_csv("models/popular_books.csv")
-book_pivot = pd.read_pickle("models/book_pivot.pkl")
-books = pd.read_pickle("models/books.pkl")
-        
-with open("models/similarity.pkl", "rb") as f:
-    similarity = pickle.load(f)
+    with open("models/similarity.pkl", "rb") as f:
+        similarity = pickle.load(f)
 
-st.success("Models loaded successfully!")
+    return popular_books, books, book_pivot, similarity
 
-st.write(popular_books.head())
+
+popular_books, books, book_pivot, similarity = load_data()
+
+
+# Add sidebar for navigation
+st.sidebar.title("📚 Navigation")
+
+option = st.sidebar.radio(
+    "Choose Recommendation Type",
+    [
+        "Popular Books",
+        "Recommend Books"
+    ]
+)
+
+# Popular Books Page
+
+
+if option == "Popular Books":
+
+    st.header("🔥 Most Popular Books")
+
+    cols = st.columns(5)
+
+    for i in range(min(20, len(popular_books))):
+
+        with cols[i % 5]:
+
+            st.image(
+                popular_books.iloc[i]["Image_URL_L"],
+                use_container_width=True
+            )
+
+            st.markdown(
+                f"**{popular_books.iloc[i]['Book_Title']}**"
+            )
+
+            st.caption(
+                popular_books.iloc[i]["Book_Author"]
+            )
+
+            st.write(
+                f"⭐ Rating: {round(popular_books.iloc[i]['avg_rating'],2)}"
+            )
+
+            st.write(
+                f"👥 {popular_books.iloc[i]['num_ratings']} Ratings"
+            )
