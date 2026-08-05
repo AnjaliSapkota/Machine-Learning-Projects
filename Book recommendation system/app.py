493025635d499c47cbf27256be5a6e8b056d9eef
AnjaliSapkota <sapkotaanjali123@gmail.com>
2026-08-05 11:45:32 +0545

    ft: loaded models(datasets)

--- Book recommendation system/app.py
+++ Book recommendation system/app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pandas as pd
+import pickle
 
 # Page Configuration
 st.set_page_config(
@@ -8,3 +10,16 @@ st.set_page_config(
 )
 
 st.write("Welcome to my Book Recommendation System!")
+
+# Load models
+
+popular_books = pd.read_csv("models/popular_books.csv")
+book_pivot = pd.read_pickle("models/book_pivot.pkl")
+books = pd.read_pickle("models/books.pkl")
+        
+with open("models/similarity.pkl", "rb") as f:
+    similarity = pickle.load(f)
+
+st.success("Models loaded successfully!")
+
+st.write(popular_books.head())
