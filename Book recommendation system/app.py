be973c47afb8cb9f8e5c9f0522f356aa53e6495c
AnjaliSapkota <sapkotaanjali123@gmail.com>
2026-08-05 11:40:15 +0545

    ft: created simple streamlit app

---
+++ Book recommendation system/app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# Page Configuration
+st.set_page_config(
+    page_title="Book Recommendation System",
+    page_icon="📚",
+    layout="wide"
+)
+
+st.write("Welcome to my Book Recommendation System!")
